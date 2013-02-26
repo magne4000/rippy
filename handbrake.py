568bf60014cd2a9db0d0de7097338b357e065f7d
Joël Charles <joel.charles91@gmail.com>
2013-02-26 17:05:21 +0100

    Parsing done

--- handbrake.py
+++ handbrake.py
@@ -4,6 +4,7 @@
 # Distributed under terms of the MIT license.
 
 import re
+from subprocess import Popen, PIPE
 
 class Stream:
     
@@ -29,60 +30,103 @@ class Stream:
 class AudioStream(Stream):
 
     re_parse = re.compile("Stream #0\.(?P<position>\d)(?:\((?P<language>.{3})\))?: \w+: (?P<codec>\w+)(?: \((?P<codecdetail>.*?)\))?, (?P<frequency>\d+) Hz, (?P<channels>\d(?:\.\d)?).*, .*?, (?P<bitrate>\d+).{1,5}(?: \((?P<default>default)\))?")
+    positions = []
 
     def parse(self):
-        """docstring for parse"""
-        pass
+        matches = AudioStream.re_parse.search(self.buf)
+        if matches is not None:
+            results = matches.groupdict()
+            self.language = results['language']
+            self.default = True if results['default'] is not None else False
+            self.channels = results['channels']
+            if results['codec'] == 'dca': #DTS
+                self.codec = results['codecdetail']
+            else:
+                self.codec = results['codec']
+            self.frequency = results['frequency']
+            self.position = results['position']
+            self.bitrate = results['bitrate']
+            AudioStream.positions.append(self.position)
+            AudioStream.positions.sort()
 
-    def getcodec(self):
-        pass
-
-    def getfrequency(self):
-        """docstring for getfrequency"""
-        pass
-
-    def getchannels(self):
-        """docstring for getchannels"""
-        pass
-
-    def getbitrate(self):
-        """docstring for getbitrate"""
-        pass
+    def getposition(self):
+        return AudioStream.positions.index(self.position)
 
 class VideoStream(Stream):
 
     re_parse = re.compile("Stream #0\.(?P<position>\d)(?:\((?P<language>.{3})\))?: \w+: (?P<codec>\w+)(?: \((?P<codecdetail>.*?)\))?, \w+, (?P<width>\d+)x(?P<height>\d+).*, (?P<fps>\d+(?:\.\d+)?) fps,.* tbc(?: \((?P<default>default)\))?")
 
-    def getcodec(self):
-        pass
-
-    def getresolution(self):
-        pass
-
-    def getfps(self):
-        """docstring for getfps"""
-        pass
-
-    def getratio(self):
-        """docstring for getratio"""
-        pass
+    def parse(self):
+        matches = VideoStream.re_parse.search(self.buf)
+        if matches is not None:
+            results = matches.groupdict()
+            self.language = results['language']
+            self.default = True if results['default'] is not None else False
+            self.fps = results['fps']
+            self.codec = results['codec']
+            self.position = results['position']
+            self.resolution = (results['width'], results['height'])
 
 class SubtitleStream(Stream):
 
-    def isforced(self):
-        pass
+    re_parse = re.compile("Stream #0\.(?P<position>\d)(?:\((?P<language>.{3})\))?: \w+: (?:.*\((?P<forced>forced)\))?")
+    positions = []
+
+    def parse(self):
+        matches = SubtitleStream.re_parse.search(self.buf)
+        if matches is not None:
+            results = matches.groupdict()
+            self.forced = True if results['forced'] is not None else False
+            self.language = results['language']
+            self.position = results['position']
+            SubtitleStream.positions.append(self.position)
+            SubtitleStream.positions.sort()
+        
+    def getposition(self):
+        return SubtitleStream.positions.index(self.position)
 
 class HandbrakeOutputParser:
 
     def __init__(self, buf):
         self.buf = buf
+        self.streams = {'audio': [], 'video': None, 'subtitle': []}
 
-    def getduration(self):
-        pass
+    def parse(self):
+        for line in self.buf.split('\n'):
+            if 'Audio: ' in line:
+                stream = AudioStream(line)
+                stream.parse()
+                self.streams['audio'].append(stream)
+            elif 'Video: ' in line:
+                stream = VideoStream(line)
+                stream.parse()
+                self.streams['video'] = stream
+            elif 'Subtitle: ' in line:
+                stream = SubtitleStream(line)
+                stream.parse()
+                self.streams['subtitle'].append(stream)
 
-    def getstreams(self):
-        pass
+    def audio(self):
+        return self.streams['audio']
 
-    def getbitrate(self):
-        """docstring for getbitrate"""
-        pass
+    def video(self):
+        return self.streams['video']
+
+    def subtitle(self):
+        return self.streams['subtitle']
+
+class HandbrakeProcess:
+
+    handbrakecli = "/usr/bin/HandBrakeCLI"
+
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.buf = None
+
+    def scan(self):
+        self.buf = self._call([HandbrakeProcess.handbrakecli, "--scan", "--input", self.filepath])
+
+    def _call(self, args):
+        child = Popen(args, stderr=PIPE)
+        child.wait()
+        return child.stderr.read()
